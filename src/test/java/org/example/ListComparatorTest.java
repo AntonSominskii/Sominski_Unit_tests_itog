2cebf94c164a31d8b5b9a8155046b2246615a3eb
AntonSominskii <101739742+AntonSominskii@users.noreply.github.com>
2024-03-10 17:07:42 +0300

    Initial commit

---
+++ src/test/java/org/example/ListComparatorTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ListComparatorTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateAverage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void compareLists() {
+    }
+}
